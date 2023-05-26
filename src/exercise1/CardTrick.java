995da456e63e0e049a523e7acda6c1a7d7a3d4b5
Kunal Gothwal <gothwalk@sheridancollege.ca>
2023-05-26 11:52:20 -0400

    second commit added

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -5,35 +5,61 @@ package exercise1;
  * It then searches the array of cards for the match to the user's card. 
  * To be used as starting code in Exercise
  *
- * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
+ * @author Kunal Gothwal May 26,2023 
  */
+import java.util.Random;
+import java.util.Scanner;
+
 public class CardTrick {
     
     public static void main(String[] args) {
-        
-        Card[] hand = new Card[7];
 
-        for (int i = 0; i < hand.length; i++) {
-            Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
-            //       Don't worry about duplicates at this point
-        }
+    Card[] hand = new Card[7];
 
-        // insert code to ask the user for Card value and suit, create their card
-        // and search the hand here. 
-        // Hint: You can ask for values 1 to 10, and then
-        //       11 for jack, 12 for queen, etc. (remember arrays are 0-based though)
-        //       1 for Hearts, 2 for Diamonds, etc. (remember arrays are 0-based though)
-        // 
-        // Then loop through the cards in the array to see if there's a match.
-        
-        // If the guess is successful, invoke the printInfo() method below.
-        
+    for (int i = 0; i < hand.length; i++) {
+        Card card = new Card();
+        card.setValue(generateRandomNumber(1, 13));
+        card.setSuit(Card.SUITS[generateRandomNumber(0, 3)]);
+        hand[i] = card;
     }
+
+    // Ask the user for card value and suit
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter the card value (1-10, 11 for Jack, 12 for Queen, 13 for King): ");
+    int value = scanner.nextInt();
+    System.out.print("Enter the card suit (1 for Hearts, 2 for Diamonds, 3 for Spades, 4 for Clubs): ");
+    int suitIndex = scanner.nextInt() - 1;
+    String suit = Card.SUITS[suitIndex];
+
+    // Search for a match in the hand
+    boolean matchFound = false;
+    for (Card card : hand) {
+        if (card.getValue() == value && card.getSuit().equals(suit)) {
+            matchFound = true;
+            break;
+        }
+    }
+
+    if (matchFound) {
+        printInfo();
+    } else {
+        System.out.println("Sorry, no match found.");
+    }
+
+    scanner.close();
+}
+
+/**
+ * Helper method to generate a random number between min and max (inclusive).
+ *
+ * @param min The minimum value.
+ * @param max The maximum value.
+ * @return The random number generated.
+ */
+private static int generateRandomNumber(int min, int max) {
+    Random random = new Random();
+    return random.nextInt(max - min + 1) + min;
+}
 
     /**
      * A simple method to print out personal information. Follow the instructions to 
